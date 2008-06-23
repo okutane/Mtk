9014e4d3eb07a991a5bd6a228c57cf36f31345ec
Octane <Octane@nnova>
2008-06-23 20:10:49 +0700

    Added all tests to AllTestsSuite

--- Matveev.Mtk.Tests/AllTestsSuite.cs
+++ Matveev.Mtk.Tests/AllTestsSuite.cs
@@ -8,7 +8,7 @@ using Matveev.Common;
 using Matveev.Mtk.Core;
 using Matveev.Mtk.Library;
 using Matveev.Mtk.Library.Fields;
-using System.Collections;
+using System.Reflection;
 
 namespace Matveev.Mtk.Tests
 {
@@ -20,7 +20,14 @@ namespace Matveev.Mtk.Tests
             get
             {
                 TestSuite suite = new TestSuite("All tests");
-                suite.Add(new YamlSerializerTest());
+
+                Assembly thatAssembly = Assembly.GetExecutingAssembly();
+                foreach (var type in thatAssembly.GetTypes())
+                {
+                    object[] attributes = type.GetCustomAttributes(typeof(TestFixtureAttribute), true);
+                    if (attributes.Length != 0)
+                        suite.Add(type.GetConstructor(System.Type.EmptyTypes).Invoke(null));
+                }
 
                 suite.Add(PolygonizationSuite.Suite);
                 return suite;
