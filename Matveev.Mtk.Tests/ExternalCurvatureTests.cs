e7007e2d8ef769b3a7485a1945be60e4fbfa164e
Octane <Octane@nnova>
2008-12-10 23:24:41 +0600

    Added tests for external curvature on sphere.

---
+++ Matveev.Mtk.Tests/ExternalCurvatureTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+using Matveev.Mtk.Core;
+using Matveev.Mtk.Library;
+using Matveev.Mtk.Library.Fields;
+
+namespace Matveev.Mtk.Tests
+{
+    [TestFixture]
+    public class ExternalCurvatureTests
+    {
+        [Test]
+        public void SphereTest()
+        {
+            Mesh mesh = MC.Instance.Create(Configuration.MeshFactory, Sphere.Sample, -1, 1, -1, 1, -1, 1, 4, 4, 4);
+            var result = from v in mesh.Vertices
+                         where VertexOps.ExternalCurvature(v) != 0
+                         select new
+                         {
+                             Vertex = v,
+                             ExternalCurvature = VertexOps.ExternalCurvature(v)
+                         };
+            YamlSerializerTest.TestSerialize("Sphere4_NonRegular.yaml", result);
+        }
+    }
+}
