79506c33113fcd53bf23038f8af11047ab6ac350
Octane <Octane@nnova>
2009-03-03 22:23:53 +0600

    Tests added.

---
+++ Matveev.Mtk.Tests/RegularityCheckTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+using Matveev.Mtk.Core;
+using Matveev.Mtk.Library;
+using Matveev.Mtk.Library.Fields;
+
+namespace Matveev.Mtk.Tests
+{
+    [TestFixture]
+    public class RegularityCheckTests
+    {
+        [Test]
+        public void Sphere3()
+        {
+            Mesh mesh = MC.Instance.Create(Configuration.MeshFactory, CompactQuadraticForm.Sphere,
+                -1, 1, -1, 1, -1, 1, 3, 3, 3);
+            foreach (Vertex v in mesh.Vertices)
+            {
+                Assert.IsTrue(VertexOps.ExternalCurvature(v) == 0, v.Point.ToString());
+            }
+            Assert.IsFalse(mesh.Vertices.Any(v => VertexOps.ExternalCurvature(v) != 0));
+        }
+    }
+}
---
+++ Matveev.Mtk.Tests/SphericalTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+using Matveev.Mtk.Core;
+using Matveev.Mtk.Library;
+
+namespace Matveev.Mtk.Tests
+{
+    [TestFixture]
+    public class SphericalTests
+    {
+        [Test]
+        public void TriangleTest()
+        {
+            Assert.AreEqual(Math.PI / 2, Spherical.PolygonArea(new Vector(1, 0, 0), new Vector(0, 1, 0),
+                new Vector(0, 0, 1)));
+        }
+
+        [Test]
+        public void FlatAngleTest()
+        {
+            double a = Math.Sqrt(2) / 2;
+            Assert.AreEqual(Math.PI / 2, Spherical.PolygonArea(new Vector(1, 0, 0), new Vector(a, a, 0),
+                new Vector(0, 1, 0), new Vector(0, 0, 1)));
+        }
+
+        [Test]
+        public void MoreFlatAnglesTest()
+        {
+            double a = Math.Sqrt(2) / 2;
+            Assert.AreEqual(Math.PI / 2, Spherical.PolygonArea(new Vector(1, 0, 0), new Vector(a, a, 0),
+                new Vector(0, 1, 0), new Vector(0, a, a), new Vector(0, 0, 1), new Vector(a, 0, a)));
+        }
+
+        [Test]
+        public void SingularTriangleTest()
+        {
+            Assert.AreEqual(0, Spherical.PolygonArea(new Vector(1, 0, 0), new Vector(1, 0, 0),
+                new Vector(0, 1, 0)));
+        }
+
+        [Test]
+        public void SphereQuarter()
+        {
+            Assert.AreEqual(Math.PI, Spherical.PolygonArea(new Vector(1, 0, 0), new Vector(-1, 0, 0),
+                new Vector(0, 1, 0)));
+        }
+    }
+}
