0a9697d7385f652e6dd72212ea3cde2c06177711
Octane <Octane@nnova>
2009-03-03 22:21:29 +0600

    Added Spherical, IFaceEnergyProvider

---
+++ Matveev.Mtk.Library/Spherical.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Matveev.Mtk.Core;
+
+namespace Matveev.Mtk.Library
+{
+    public static class Spherical
+    {
+        public static double PolygonArea(params Vector[] vectors)
+        {
+            int n = vectors.Length;
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Vector p = Vector.Normalize(vectors[i] ^ vectors[(i + n - 1) % n]);
+                Vector q = Vector.Normalize(vectors[i] ^ vectors[(i + 1) % n]);
+                sum += Math.Acos(p * q);
+            }
+            return sum - (n - 2) * Math.PI;
+        }
+    }
+}
