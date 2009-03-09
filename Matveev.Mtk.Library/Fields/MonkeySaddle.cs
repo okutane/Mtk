5740cbdfaed1f4ad2fa710902c05418f554fbaec
Octane <Octane@nnova>
2009-03-09 13:03:07 +0600

    New surfaces

---
+++ Matveev.Mtk.Library/Fields/MonkeySaddle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Matveev.Mtk.Core;
+
+namespace Matveev.Mtk.Library.Fields
+{
+    public class MonkeySaddle : IImplicitSurface
+    {
+        #region IImplicitSurface Members
+
+        public double Eval(Point p)
+        {
+            return p.Z - Math.Pow(p.X, 3) + 3 * Math.Pow(p.Y, 2) * p.X;
+        }
+
+        public Vector Grad(Point p)
+        {
+            return new Vector();
+        }
+
+        #endregion
+
+        public static readonly MonkeySaddle Instance = new MonkeySaddle();
+    }
+}
---
+++ Matveev.Mtk.Library/Fields/PiriformTeardrop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Matveev.Mtk.Core;
+
+namespace Matveev.Mtk.Library.Fields
+{
+    public class PiriformTeardrop : IImplicitSurface
+    {
+        #region IImplicitSurface Members
+
+        public double Eval(Point p)
+        {
+            double x3 = p.X * p.X * p.X;
+            double x4 = x3 * p.X;
+            return x4 - x3 + p.Y * p.Y + p.Z * p.Z;
+        }
+
+        public Vector Grad(Point p)
+        {
+            return new Vector(4 * Math.Pow(p.X, 3) + 3 * Math.Pow(p.X, 2), 2 * p.Y, 2 * p.Z);
+        }
+
+        #endregion
+
+        public static readonly PiriformTeardrop Instance = new PiriformTeardrop();
+    }
+}
