2b48b0231d92a16af93f1686d1182fd6b3f46881
Dmitry Matveev <okutane@users.noreply.github.com>
2009-03-22 12:17:27 +0600

    Initial implementation of "dynamic mesh optimization"

---
+++ Matveev.Mtk.Library/Utilities/UFace.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Matveev.Mtk.Core;
+
+namespace Matveev.Mtk.Library.Utilities
+{
+    public static class UFace
+    {
+        public static double Area(this Face face)
+        {
+            Point[] points = face.Vertices.Select(v => v.Point).ToArray();
+            return points[0].AreaTo(points[1], points[2]);
+        }
+    }
+}
